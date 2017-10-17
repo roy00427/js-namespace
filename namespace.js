8b3f08220671e87161b52c90138e75b1fd63605f
Protimaroy <32514552+Protimaroy@users.noreply.github.com>
2017-10-17 13:34:21 -0400

    add

---
+++ namespace.js
@@ -0,0 +1,23 @@
+var ROY00427 = {
+    init: function () {
+        console.log("script in ROY00427 called");
+        var div = document.createElement("div");
+        
+        div.className = "box";
+        div.textContent = "ROY00427";
+        
+        document.getElementById("boxes").appendChild(div);
+        
+        div.addEventListener("click", F1);
+        div.addEventListener("mouseover", F2);
+        div.addEventListener("mouseout", F2);
+        
+        function F2(ev){
+            this.classList.toggle("highlight");
+        }
+        function F1(ev)  {
+            this.style.backgroundColor = "aqua";
+            this.style.backgroundColor = "Blue";
+        }
+    }
+}
